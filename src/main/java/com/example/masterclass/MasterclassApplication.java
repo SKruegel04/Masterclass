e6797ce5cbf906a76d4b9c21ef1fb3d82cbe05b0
Simone Kruegel <simone.kruegel04@gmail.com>
2022-10-13 00:29:12 +0100

    added controller and html

--- src/main/java/com/example/masterclass/MasterclassApplication.java
+++ src/main/java/com/example/masterclass/MasterclassApplication.java
@@ -1,4 +1,4 @@
-package com.example.Masterclass;
+package com.example.masterclass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/masterclass/MasterclassApplicationTests.java
+++ src/test/java/com/example/masterclass/MasterclassApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Masterclass;
+package com.example.masterclass;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
